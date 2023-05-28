b95e4ec6e3d9b69d317c897fb7470f73173c77d1
Fabrizio Verde <signoredellarete@gmail.com>
2023-05-29 00:39:57 +0200

    Update installation with kubectl-neat download

--- install.sh
+++ install.sh
@@ -1,2 +1,60 @@
 #!/bin/bash
-cp -rp bin/* /usr/local/bin/
+download=${download_wget}
+download_wget='wget'
+download_curl='curl -O'
+
+echo_off() {
+  exec 3>&1-
+  exec 4>&2-
+}
+
+echo_on() {
+  exec 1>&3-
+  exec 2>&4-
+}
+
+check_bin() {
+  echo_off
+  ${1}
+  result=$?
+  echo_on
+  if [ ${result} -ne 127 ];then
+    true
+  else
+    false
+  fi
+}
+
+if check_bin "wget"; then
+  download=${download_wget}
+elif check_bin "curl"; then
+  download=${download_curl}
+else
+  echo "Error: No download command found!"
+  echo "Please install wget or curl."
+  exit 1
+fi
+
+# Download kubectl-neat
+mkdir bin/tmp
+cd bin/tmp
+echo "Downloading latest release of kubectl-neat from github..."
+
+${download} https://github.com/itaysk/kubectl-neat/releases/latest/download/kubectl-neat_linux_amd64.tar.gz > /dev/null 2>&1
+tar xzf kubectl-neat_linux_amd64.tar.gz
+mv kubectl-neat ..
+cd ..
+rm -rf tmp
+
+# Installation
+echo "Installing binaries..."
+echo " "
+for i in `ls -1`
+do
+  if sudo cp ${i} /usr/local/bin/${i}; then
+    echo "/usr/local/bin/${i}"
+  fi
+done
+
+echo " "
+echo "Installation done!"
